ecd29f621ba28d08af8b66f8d722abd4424624eb
Can Wang <canw7@uci.edu>
2022-01-11 01:24:31 -0800

    done task 2 & task 3

--- src/main/java/MileStone1/MileStone1.java
+++ src/main/java/MileStone1/MileStone1.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class MileStone1 {
+    private final static int INDENT_SPACE = 4;
 
     /* Task 1 read XML file to json object, then save the json file in local disk */
     private static JSONObject getJsonObjectFromXMLFile(String xmlFilePath) {
@@ -31,24 +32,76 @@ public class MileStone1 {
     private static void writeJsonObjectToLocalDisk(JSONObject jsonObject, String jsonFilePath) {
         try {
             FileWriter fw = new FileWriter(jsonFilePath);
-            fw.write(jsonObject.toString());
+            fw.write(jsonObject.toString(INDENT_SPACE));
             fw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(jsonObject);
     }
 
+    private static void writeJsonStringToLocalDisk(String jsonString, String jsonFilePath) {
+        try {
+            FileWriter fw = new FileWriter(jsonFilePath);
+            fw.write(jsonString);
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    private static void querySubJsonFromXMLFile(String XMLFilePath, String queryString,
+                                                      String targetJsonFilePath) {
+        JSONObject jsonObject = getJsonObjectFromXMLFile(XMLFilePath);
+        Object subJsonObject = null;
+        String jsonResultString;
+
+        try {
+            JSONPointer ptr = new JSONPointer(queryString);
+            subJsonObject = ptr.queryFrom(jsonObject);
+
+        } catch (JSONPointerException e) {
+            e.printStackTrace();
+        }
+
+        if (subJsonObject == null) {
+            System.out.println("** The Given Key Path Did Not Match, Discard The Query! **");
+            return;
+        }
+
+        // different cast with given type
+        if (subJsonObject instanceof JSONArray) {
+            // type1: JSONArray
+            jsonResultString = ((JSONArray) subJsonObject).toString(INDENT_SPACE);
+        }
+        else if (subJsonObject instanceof JSONObject) {
+            // type2: JSONObject
+            jsonResultString = ((JSONObject) subJsonObject).toString(INDENT_SPACE);
+        }
+        else {
+            // type3: String or value(int, float, bigdecimal...)
+            jsonResultString = subJsonObject.toString();
+        }
+
+        // write back to disk
+        System.out.println("query result class: " + subJsonObject.getClass());
+        System.out.println("query result: \n" + jsonResultString);
+        writeJsonStringToLocalDisk(jsonResultString, targetJsonFilePath);
+    }
 
     public static void main(String[] args) {
 
         // Task 1
-        JSONObject jsonObject = getJsonObjectFromXMLFile("./testcase/books.xml");
+        JSONObject jsonObject;
+        jsonObject = getJsonObjectFromXMLFile("./testcase/books.xml");
         writeJsonObjectToLocalDisk(jsonObject, "./testcase/output/books.json");
 
 
-        //
+        // Task 2 & Task 3
+        querySubJsonFromXMLFile("./testcase/books.xml",
+                "/catalog/book/4","./testcase/output/books_query_result.json");
+        //writeJsonObjectToLocalDisk();
+
 
     }
 }
